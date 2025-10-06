ce9ccc6eff044f7b2fabde2ade6e229eb7a39cb2
AndreaSantarsiero <andreasantarsiero03@gmail.com>
2025-10-06 22:07:22 +0200

    nothing changed

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,9 +1,11 @@
 export function log(...args: any[]) {
   console.log("[netflix-sync]", ...args);
 }
+
 export function warn(...args: any[]) {
   console.warn("[netflix-sync]", ...args);
 }
+
 export function error(...args: any[]) {
   console.error("[netflix-sync]", ...args);
 }
